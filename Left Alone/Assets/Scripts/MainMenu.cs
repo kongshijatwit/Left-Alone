ef66fb9ed98f96829334c73e0d1083693d8c2b2a
Solovey <77858446+soloveyg@users.noreply.github.com>
2023-07-30 13:26:59 -0400

    Scene is not loading.
    Added font, background, script, and created a scene

---
+++ Left Alone/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject BackButton;
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoToSettingsMenu()
+    {
+        BackButton.SetActive(true);
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
